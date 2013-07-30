00e93b24a27a38289ee42c87cd85dac62226d167
Rob Ashton <robashton@codeofrob.com>
2013-07-30 20:49:38 +0100

    Force lein

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject adventures "0.1.0-SNAPSHOT"
   :repositories {"sonatype-oss-public"
                  "https://oss.sonatype.org/content/groups/public/"}
+
+  :min-lein-version "2.2.0"
   
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-http "0.7.6"]
