e4402ed02a4e19351405753b913859cbb8d3a404
James Henderson <james@pcr.me.uk>
2013-07-30 19:48:17 +0100

    Adding deps

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject adventures "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :repositories {"sonatype-oss-public"
+                 "https://oss.sonatype.org/content/groups/public/"}
+  
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clj-http "0.7.6"]
+                 [org.clojure/core.async "0.1.0-SNAPSHOT"]])
